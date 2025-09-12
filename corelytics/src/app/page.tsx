6f0da20a894f1c65b8b184760aeae37e97bed148
tomex <64604910+tomex1337@users.noreply.github.com>
2025-09-12 13:39:58 +0200

    fix: speedinsight áthelyezése

--- corelytics/src/app/page.tsx
+++ corelytics/src/app/page.tsx
@@ -6,6 +6,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   const { data: session } = useSession();
+    <SpeedInsights/>
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
