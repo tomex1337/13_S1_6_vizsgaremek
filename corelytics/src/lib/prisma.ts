647ffa3fe6085f8bd446a41b9981d1f8180d4af5
tomex <64604910+tomex1337@users.noreply.github.com>
2025-09-16 13:21:01 +0200

    feat: Prisma Accelator hozzáadása

--- corelytics/src/lib/prisma.ts
+++ corelytics/src/lib/prisma.ts
@@ -1,11 +1,8 @@
 import { PrismaClient } from '../generated/prisma';
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma;
-}
+export const prisma = new PrismaClient().$extends(withAccelerate());
