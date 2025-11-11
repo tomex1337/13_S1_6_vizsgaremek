7b9a93912a0ba508d398518210dcbe4e4a78c3fb
tomex <64604910+tomex1337@users.noreply.github.com>
2025-11-11 09:08:57 +0100

    fix: duplikált listák kitörlése

--- corelytics/src/components/header.tsx
+++ corelytics/src/components/header.tsx
@@ -37,17 +37,6 @@ const callsToAction = [
   { name: 'Kapcsolatfelvétel', href: '#', icon: PhoneIcon },
 ]
 
-const products = [
-  { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
-  { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
-  { name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
-  { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
-  { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
-]
-const callsToAction = [
-  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-  { name: 'Contact sales', href: '#', icon: PhoneIcon },
-]
 const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 const { data: session } = useSession();
 
