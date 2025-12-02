c38e03172c13cbf41ba16651679a6d89c9760e5f
Mészáros Imre <118882031+xXImrePVPXx@users.noreply.github.com>
2025-12-02 11:45:17 +0100

    feat: header sötét mód, főoldal kezdete

--- corelytics/src/app/page.tsx
+++ corelytics/src/app/page.tsx
@@ -15,34 +15,20 @@ export default function Home() {
       <Header />
       <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        {session?.user ? (
-          <div className="flex flex-col items-center sm:items-start gap-4 mb-4">
-            <div className="text-2xl font-semibold">
-              Üdvözlünk vissza, {session.user.name || session.user.email}! 👋
+          <div className="flex flex-col sm:flex-row gap-12 items-center sm:items-center">
+            <div className="text-3x1 md:text-2x1 sm:text-6xl font-semibold sm:w-2/3 text-center sm:text-left">
+              Gondold újra<br/>az edzésterved
             </div>
-            <button
-              onClick={() => signOut()}
-              className="px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 transition-colors"
-            >
-              Kijelentkezés
-            </button>
+            <div className="flex justify-center w-full sm:w-1/3 sm:shrink-0 bg-white dark:bg-gray-900 rounded-xl shadow-lg p-4">
+              <Image
+                src="/logo.svg"
+                alt="Corelytics Logo"
+                width={250}
+                height={250}
+              >
+              </Image>
+            </div>
           </div>
-        ) : (
-          <div className="flex flex-col sm:flex-row items-center gap-4 mb-4">
-            <a
-              href="/auth/signin"
-              className="w-32 text-center px-6 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 transition-colors"
-            >
-              Bejelentkezés
-            </a>
-            <a
-              href="/auth/signup"
-              className="w-32 text-center px-6 py-2 border border-indigo-600 text-indigo-600 rounded-md hover:bg-indigo-50 transition-colors"
-            >
-              Regisztráció
-            </a>
-          </div>
-        )}
         <Image
           className="dark:invert"
           src="/next.svg"
