de65a43201e5a80be694d6a81dedb0ab4ce222bf
Mészáros Imre <118882031+xXImrePVPXx@users.noreply.github.com>
2025-12-05 13:05:50 +0100

    fix: kettő input mező formázása

--- corelytics/src/app/auth/signup/page.tsx
+++ corelytics/src/app/auth/signup/page.tsx
@@ -111,7 +111,7 @@ export default function SignUp() {
                 id="password"
                 {...register("password", { required: "A jelszó megadása kötelező" })}
                 type="password"
-                className=""
+                className="relative block w-full p-2 rounded-md border-1 border-indigo-900 py-1.5 text-gray-900 dark:border-indigo-200 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 placeholder="Jelszó"
               />
               {errors.password && (
